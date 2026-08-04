1ffc4bf5fbdd90f3829dbd716e61d84ea6eb1030
androcatz1 <narraj2381@gmail.com>
2026-08-04 20:33:00 +0800

    added rate limiting function by getting client ip

---
+++ app/core/limiter.py
@@ -0,0 +1,10 @@
+from fastapi import Request
+from slowapi import Limiter
+
+def get_client_ip(request: Request):
+    forwarded = request.headers.get("X-Forwarded-For")
+    if forwarded:
+        return forwarded.split(",")[0].strip()
+    return request.client.host
+
+limiter = Limiter(key_func=get_client_ip)
