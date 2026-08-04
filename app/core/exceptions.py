0aaf42bd70b43ecc085287fc9e69fa125b680659
androcatz1 <narraj2381@gmail.com>
2026-08-04 20:32:17 +0800

    added rate limit handler

---
+++ app/core/exceptions.py
@@ -0,0 +1,14 @@
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from slowapi.errors import RateLimitExceeded
+
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=429,
+        content={
+            "detail": {
+                "status": "error",
+                "message": "Too many requests. Please try again later."
+            }
+        }
+    )
